5e32b3599e36809bba9d77068a6988e6d6ae7e2b
mungsehari <mungse.hari@gmail.com>
2025-01-11 12:19:12 +0530

    add: authMiddleware

---
+++ middelwares/authMiddleware.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+
+const authenticateJWT = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+      if (err) {
+        return res.sendStatus(403);
+      }
+      req.user = user;
+      next();
+    });
+  } else {
+    res.sendStatus(401);
+  }
+};
+
+export default authenticateJWT;
